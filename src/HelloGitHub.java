3b0b28e3ce4340ada04d29bb9c66b1ebb427c912
Oliver Suess <sueolb05@gmail.com>
2015-09-08 20:05:27 +0200

    Dev 2 commit

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -3,7 +3,7 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 	
-		System.out.println("Hello GitHub, I'm developer 2!");
+		System.out.println("Hello GitHub, I'm developer 2!!");
 
 	}
 
